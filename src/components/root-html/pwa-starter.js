b115cdd08c3b246ee5d1566f9ab3582dee38bae2
Ahmet Can <ahmetcanguven44@gmail.com>
2017-12-30 15:53:49 +0300

    Fix SW loader

--- src/components/root-html/pwa-starter.js
+++ src/components/root-html/pwa-starter.js
@@ -1,7 +1,7 @@
 if (navigator.serviceWorker.controller) {
   console.log('PWA active service worker found, no need to register')
 } else {
-  navigator.serviceWorker.register('sw.js', {
+  navigator.serviceWorker.register('/sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
